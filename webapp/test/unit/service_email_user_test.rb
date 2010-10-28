35d61bb9703ddeb6a0130210b0100c3e98272709
Joel Cogen <joel@joelcogen.com>
2010-10-28 09:42:55 +0200

    Typo in header (LGPL)

--- webapp/test/unit/service_email_user_test.rb
+++ webapp/test/unit/service_email_user_test.rb
@@ -5,7 +5,7 @@
 # Copyright (C) 2010 Joel Cogen <http://joelcogen.com>
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License
+# You should have received a copy of the GNU General Public License
 # along with this module. If not, see <http://www.gnu.org/licenses/>.
 #
 # This program incorporates work covered by the following copyright and
